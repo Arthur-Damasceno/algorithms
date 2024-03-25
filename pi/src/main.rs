232b3394f4d8b08865a2ede6ee2d3b537667c56a
Arthur Damasceno <damascenoarthur653@gmail.com>
2024-03-25 08:46:34 -0300

    feat: `pi`

---
+++ pi/src/main.rs
@@ -0,0 +1,29 @@
+use std::io::{Write, stdin, stdout};
+
+fn read_operations() -> u32 {
+    let mut x = String::new();
+
+    print!("How many operations?: ");
+    stdout().flush().unwrap();
+
+    stdin().read_line(&mut x).expect("Error while reading number");
+
+    x.trim().parse().expect("Invalid number")
+}
+
+fn main() {
+    let operations = read_operations();
+    let mut pi = 0.;
+
+    for i in 0..operations {
+        let x = if i % 2 == 0 {
+            4. / (1. + 2. * (i as f64))
+        } else {
+            -4. / (1. + 2. * (i as f64))
+        };
+
+        pi += x;
+    }
+
+    println!("The approximate value of pi is {pi}")
+}
